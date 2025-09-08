c73951e6900a1dfb919339d00f8dbd8616daf3ba
rohanyog <rohanrajput0523@gmail.com>
2025-09-08 09:36:52 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Products;
+using System.Linq;
+using System.Web.Mvc;
+
+public class HomeController : Controller
+{
+    private ProductEntities  db = new ProductEntities();
+
+    public ActionResult Index(string searchString)
+    {
+        var products = db.Products.Include("Category").AsQueryable();
+
+        // If search string is not empty, filter
+        if (!string.IsNullOrEmpty(searchString))
+        {
+            products = products.Where(p =>
+                p.ProductName.Contains(searchString) ||
+                p.Category.CategoryName.Contains(searchString));
+        }
+
+        // Execute query
+        return View(products.ToList());
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Products")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Products")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("44722042-6d17-4cd3-9847-f4d7261e52ec")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
